08cdfa28b9b1e3425452b7c3fed159db34ce6ac9
Gustavo Muraoka <gustavomuraoka15@gmail.com>
2024-04-23 08:41:47 -0300

    Finished all HTML pages

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/RI")
+def RI():
+    return render_template("RI.html")
+
+@app.route("/aboutus")
+def aboutus():
+    return render_template("aboutus.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
