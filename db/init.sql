8d620a24c88af2fd148355f7eaa2b179deb53f46
Gustavo Muraoka <gustavomuraoka15@gmail.com>
2024-05-08 13:06:08 -0300

    Dockerized

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS CSVRegister;
-USE CSVRegister;
+CREATE DATABASE IF NOT EXISTS RI;
+USE RI;
 
 /* CREATE TABLE IF NOT EXISTS RegistroIndividual (
 id_reg int primary key not null auto_increment,
@@ -12,12 +12,8 @@ temperatura_reg double not null,
 volume_reg double not null
 ); */
 
-
-
-CREATE TABLE IF NOT EXISTS Estufa(
-    est_dh datetime not null,
-    est_um_solo double not null,
-    est_um_amb double not null,
-    est_temp double not null,
-    est_vol_aq double not null
+CREATE TABLE IF NOT EXISTS Usuario(
+    id_user int primary key auto_increment not null,
+    user_nome varchar(30) not null,
+    user_email varchar(30) not null
 );
